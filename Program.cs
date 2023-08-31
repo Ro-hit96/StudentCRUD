4e98d3de25a3dee1045060583ef2d44645c27f2c
RO-HITT <rsurvase1132@gmail.com>
2023-08-31 23:53:22 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentCRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int no = 0;
+
+            GetStudent crud = new GetStudent();
+            do
+            {
+
+                Console.WriteLine("1.Student List");
+                Console.WriteLine("2.Get Student by Id");
+                Console.WriteLine("3.Add New Student");
+                Console.WriteLine("4.Update Student");
+                Console.WriteLine("5.Delet Sudent");
+                Console.WriteLine("Enter Your Openion");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        List<Student> list = crud.GetStudentList();
+                        Console.WriteLine("RollNo\tName\t Percentage\tDepartment");
+                        foreach (Student s in list)
+                        {
+                            Console.WriteLine($"{s.RollNo}     {s.Name}       {s.Per}         {s.Department}");
+                        }
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter the Roll No you want to Display");
+                        int rollno = Convert.ToInt32(Console.ReadLine());
+                        Student s1 = crud.GetStudentbyRollNo(rollno);
+                        Console.WriteLine("RollNo\tName\t Percentage\tDepartment");
+                        Console.WriteLine($"{s1.RollNo}     {s1.Name}       {s1.Per}         {s1.Department}");
+
+                        break;
+
+                    case 3:
+                        Student s2 = new Student();
+                        Console.WriteLine("Enter The roll no of the Student");
+                        s2.RollNo = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter The Student Name");
+                        s2.Name = Console.ReadLine();
+                        Console.WriteLine("Enter The Percentage");
+                        s2.Per = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Department");
+                        s2.Department = Console.ReadLine();
+                        crud.AddStudent(s2);
+                        break;
+
+                    case 4:
+                        Student s3 = new Student();
+                        Console.WriteLine("Enter the Rollno");
+                        s3.RollNo = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the Name");
+                        s3.Name = Console.ReadLine();
+                        Console.WriteLine("Enter the Persentage");
+                        s3.Per = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter the Department");
+                        s3.Department = Convert.ToString(Console.ReadLine());
+                        crud.UpdateStudent(s3);
+                        break;
+
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter Roll No that you want to Delete");
+                        int RollNo = Convert.ToInt32(Console.ReadLine());
+                        crud.DeleteStudent(RollNo);
+                        Console.WriteLine($"{RollNo} Record Deleted Sucessfully");
+                        break;
+                }
+                Console.WriteLine("Press One for Continue");
+                no = Convert.ToInt32(Console.ReadLine());
+
+            } while (no == 1);
+       } 
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentCRUD
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public double Per { get; set; }
+        public string Department { get; set; }
+
+    }
+    public class GetStudent
+    {
+        private List<Student> Studentlist;
+        private List<Student> studentList;
+
+        public GetStudent()
+        {
+            studentList = new List<Student>()
+            {
+                new Student {RollNo=001,Name="Sakshi",Per=80.33,Department="Error Solveing"},
+                new Student { RollNo = 002, Name = "Vinayak", Per = 50,Department="Timepass" },
+                new Student { RollNo=003,Name="Anata",Per=60,Department="Electric"}
+            };
+        }
+        public List<Student> GetStudentList()
+        {
+           return studentList;
+        }
+
+        public Student GetStudentbyRollNo(int rollNo)
+        {
+            Student student = new Student();
+            foreach (Student s2 in studentList)
+            {
+                if(s2.RollNo == rollNo)
+                {
+                    student = s2;
+                }
+            }
+            return student;
+        }
+        public void AddStudent(Student s)
+        {
+            studentList.Add(s);
+        }
+
+        public void UpdateStudent(Student p)
+        {
+            foreach (Student s2 in studentList)
+            {
+                if (s2.RollNo == p.RollNo)
+                {
+                    s2.Name= p.Name;
+                    s2.Per= p.Per;
+                    s2.Department= p.Department;
+                    break;
+                }
+            }
+            Console.WriteLine("Updated Data Saved");
+        }
+        public void DeleteStudent(int rollNo)
+        {
+            foreach (Student s in studentList)
+            {
+                if (s.RollNo == rollNo)
+                {
+                    studentList.Remove(s);
+                    break;
+                }
+            }
+        }
+
+        
+    }
+}
